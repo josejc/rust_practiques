7fa86af1e44847ab07ee0a9debfccefeb83cf115
José J. Cabezas <josej.cabezas@gmail.com>
2018-05-08 18:14:43 +0200

    Improve word counts

--- word_counts/src/main.rs
+++ word_counts/src/main.rs
@@ -1,13 +1,18 @@
 use std::io;
+use std::io::Read;
 
 fn main() {
     let mut entrada = String::new();
 
-    io::stdin().read_line(&mut entrada).expect(
+    io::stdin().read_to_string(&mut entrada).expect(
         "Error al llegir l'entrada",
     );
-    
+
+    let m_lineas = entrada.lines().enumerate();
     let paraules: Vec<&str> = entrada.split_whitespace().collect();
-    println!("{:?}", paraules);
+    let n_lineas: Vec<&str> = entrada.split_terminator("\n").collect();
+    println!("Nombre n_lineas: {}", n_lineas.iter().count());
+    println!("Nombre m_lineas: {}", m_lineas.count());
     println!("Nombre paraules: {}", paraules.iter().count());
+    println!("Nombre bytes: {}", entrada.bytes().count());
 }
