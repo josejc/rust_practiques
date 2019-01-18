58bb45fc0a4fb12d9384b20c320eb436a50e7add
José J. Cabezas <josej.cabezas@gmail.com>
2019-01-18 13:15:22 +0100

    LCG with function and struct

--- lcg/src/main.rs
+++ lcg/src/main.rs
@@ -1,26 +1,37 @@
 use std::io;
 use std::io::Write; // <--- bring flush() into scope
 
+pub struct LCG {
+    x: u32,
+    a: u32,
+    c: u32,
+    m: u32,
+}
+
+pub fn rand(r: &mut LCG) -> f32 {
+    //x = ((a * x) + c ) % m; Panic multiply with overflow
+    let mut aux: u64;       // Solution to overflow of u32 
+
+    aux = r.a as u64 * r.x as u64;
+    aux = aux + r.c as u64;
+    aux = aux % r.m as u64;
+    r.x = aux as u32;
+    (r.x as f32 / r.m as f32)
+}
+
 fn main() {
     println!("Linear Congruential Generator: Xn+1 = (aXn + c) mod m");
 
     let mut entrada = String::new();
-    let mut x: u32;
-    let mut u: f32;
-    let mut aux: u64;   // Auxiliar per evitar overflow 
-    let a: u32;
-    let c: u32;
-    let m: u32;
     let n: u32; 
+    let mut r = LCG { x: 1_973_272_912, a: 630_360_016, c: 0, m: 2_147_483_647};
 
     print!("X0? [1.973.272.912] ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut entrada).expect("Error al llegir l'entrada",);
     entrada.pop();          // Remove CR last char ;)
-    if entrada.is_empty() {
-        x = 1_973_272_912;
-    } else {
-        x = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
+    if !entrada.is_empty() {
+        r.x = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
     }
   
     entrada.clear();
@@ -28,10 +39,8 @@ fn main() {
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut entrada).expect("Error al llegir l'entrada",);
     entrada.pop();
-    if entrada.is_empty() {
-        a = 630_360_016;
-    } else {
-        a = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
+    if !entrada.is_empty() {
+        r.a = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
     }
 
     entrada.clear();
@@ -39,10 +48,8 @@ fn main() {
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut entrada).expect("Error al llegir l'entrada",);
     entrada.pop();
-    if entrada.is_empty() {
-        c = 0;
-    } else {
-        c = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
+    if !entrada.is_empty() {
+        r.c = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
     }
 
     entrada.clear();
@@ -50,10 +57,8 @@ fn main() {
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut entrada).expect("Error al llegir l'entrada",);
     entrada.pop();
-    if entrada.is_empty() {
-        m = 2_147_483_647;  // 2^31-1
-    } else {
-        m = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
+    if !entrada.is_empty() {
+        r.m = entrada.trim().parse().expect("No és un nombre sencer [u32]",);
     }
 
     entrada.clear();
@@ -68,13 +73,7 @@ fn main() {
     }
 
     for _i in 0..n {
-        //x = ((a * x) + c ) % m; Panic multiply with overflow
-        aux = a as u64 * x as u64;
-        aux = aux + c as u64;
-        aux = aux % m as u64;
-        x = aux as u32;
-        u = x as f32 / m as f32;
-        print!("{:.6}, ", u);
+        print!("{:.6}, ", rand(&mut r));
     }
     println!("...");
 }
