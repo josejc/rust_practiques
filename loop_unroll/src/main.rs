94416df85d32dd5e3d9271e299eff4a4adf94836
José J. Cabezas <josej.cabezas@gmail.com>
2019-02-26 20:59:57 +0100

    Generic unroll n loops

--- loop_unroll/src/main.rs
+++ loop_unroll/src/main.rs
@@ -14,4 +14,24 @@ fn main() {
         println!("{},{}", i,j);
         x += 1;
     }
+    println!("4 loops of range 4 -> 256");    
+    for a in 1..5 {
+        for b in 1..5 {
+            for c in 1..5 {
+                for d in 1..5 {
+                    println!("{},{},{},{}", a,b,c,d);
+                }
+            } 
+       }
+    }
+    println!("1 loop of range 4*4*4*4 (256)");
+    let mut x = 0;
+    while x < 256 {
+        let d = (x%4)+1;    // Remainder
+        let c = ((x/4)%4)+1;
+        let b = ((x/16)%4)+1;
+        let a = ((x/64)%4)+1;   
+        println!("{},{},{},{}", a,b,c,d);
+        x += 1;
+    }        
 }
