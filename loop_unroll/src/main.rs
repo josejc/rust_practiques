555e764af6cf25e32df06a0b6228ebdf4aeb13fe
José J. Cabezas <josej.cabezas@gmail.com>
2019-02-27 21:24:55 +0100

    Add checks for indexes with generic formulas

--- loop_unroll/src/main.rs
+++ loop_unroll/src/main.rs
@@ -33,5 +33,21 @@ fn main() {
         let a = ((x/64)%4)+1;   
         println!("{},{},{},{}", a,b,c,d);
         x += 1;
-    }        
+    }
+    println!("1 loop of range 4*4*4*4 (256) amb array with formula");
+    for x in 1..257 {
+        let mut e: [i32; 5] = [0; 5];
+        for i in 1..5 {
+            e[i] = ((x/4_i32.pow((i as u32)-1))%4)+1;
+        } 
+        println!("{},{},{},{}", e[4],e[3],e[2],e[1]);
+    }    
+    // Check loops for generate index (function equality - permutation)
+    println!("Index for equality in permutation");
+    // i -> 1..n-1, j ->i+1..n
+    for i in 1..4 {
+        for j in i+1..5 {
+            println!("{},{}",i,j);
+        }
+    }
 }
