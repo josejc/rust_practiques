c15ce2b00b11d3224e1ed6a3f79a76a5dc0844c8
José J. Cabezas <josej.cabezas@gmail.com>
2020-04-13 16:34:47 +0200

    Resolve Sudoku if all positions have only one possibility

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -13,12 +13,12 @@ fn pendent_fila(f: [u8; N]) -> HashSet<u8> {
     return diff;
 }
 
-fn pendent_columna(s: [[u8; N]; N], col: u8) -> HashSet<u8> {
+fn pendent_columna(s: [[u8; N]; N], col: usize) -> HashSet<u8> {
     let p: HashSet<u8> =  [1, 2, 3, 4, 5, 6, 7, 8, 9].iter().cloned().collect();    // Conjunt de possibilitats d'una cel.la
 
     let mut c_set: HashSet<u8> = HashSet::new();
     for i in 0..N {
-        c_set.insert(s[i][0]);
+        c_set.insert(s[i][col]);
     }
     //println!("columna: {:?}",c_set  );
     let diff: HashSet<u8> = p.difference(&c_set).cloned().collect();
@@ -26,6 +26,37 @@ fn pendent_columna(s: [[u8; N]; N], col: u8) -> HashSet<u8> {
 
     return diff;    
 }
+
+fn pendent_sub(s: [[u8; N]; N], row: usize, col: usize) -> HashSet<u8> {
+    let p: HashSet<u8> =  [1, 2, 3, 4, 5, 6, 7, 8, 9].iter().cloned().collect();    // Conjunt de possibilitats d'una cel.la
+
+    let mut s_set: HashSet<u8> = HashSet::new();
+    let mut i = 6..9;
+
+    if row < 3 {
+        i = 0..3;
+    } else if row < 6 {
+        i = 3..6;
+    }
+
+    for x in i {
+        let mut j = 6..9;
+        if col < 3 {
+            j = 0..3;
+        } else if col < 6 {
+            j = 3..6;
+        }        
+        for y in j {      
+            s_set.insert(s[x][y]);
+        } 
+    }
+    //println!("sub: {:?}",s_set  );
+    let diff: HashSet<u8> = p.difference(&s_set).cloned().collect();
+    //println!("pendents: {:?}", diff);
+
+    return diff; 
+}
+
 
 fn main() {
     
@@ -82,7 +113,59 @@ fn main() {
     //pendent_fila(sudoku[0]);
     //pendent_fila(sudoku[8]);
 
-    //pendent_columna(sudoku, 0);
+    //pendent_columna(sudoku, 1);
+    //pendent_columna(sudoku, 2); 
 
-    //pendent_sub(sudoku, 0, 0); 
+    //pendent_sub(sudoku, 3, 4);
+    let mut fi = false;
+    let mut complet = true;
+    while !fi {
+        complet = true;
+        let mut canvi = false;
+        for i in 0..9 {
+            for j in 0..9 {
+                if sudoku[i][j] == 0 {
+                    let f = pendent_fila(sudoku[i]);    //i
+                    let c = pendent_columna(sudoku, j); //j
+                    let s = pendent_sub(sudoku, i, j);  //i,j
+                    let m: HashSet<u8>  = f.intersection(&c).cloned().collect();
+                    //println!("Mig: {:?}", m);
+                    let mut p: HashSet<u8> = m.intersection(&s).cloned().collect();
+                    //println!("Possibilitats: {:?}", p);
+                    if p.len() == 1 {
+                        let number: u8 = p.drain().next().unwrap();
+                        //println!("OK {}", number);
+                        sudoku[i][j] = number;
+                        canvi = true;
+                    } else {
+                        complet = false;
+                    }
+                }
+            }
+        }
+        if !canvi {
+            fi = true;
+        }
+    }
+
+    println!("Final:");
+    for (_i, row) in sudoku.iter().enumerate() {
+        println!("{:?}", row);
+    }
+
+    if !complet {
+        println!("Sudoku no finalitzat...");
+        for i in 0..9 {
+            for j in 0..9 {
+                if sudoku[i][j] == 0 {
+                    let f = pendent_fila(sudoku[i]);    //i
+                    let c = pendent_columna(sudoku, j); //j
+                    let s = pendent_sub(sudoku, i, j);  //i,j
+                    let m: HashSet<u8>  = f.intersection(&c).cloned().collect();
+                    let p: HashSet<u8> = m.intersection(&s).cloned().collect();
+                    println!("Possibilitats posició {}, {}: {:?}", i, j, p);
+                }
+            }
+        }        
+    }    
 }
