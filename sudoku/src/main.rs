efd1355b8bb11d3feb5fbd93be27cea0e068d0a6
José J. Cabezas <josej.cabezas@gmail.com>
2020-04-14 20:55:53 +0200

    Add function of restrictions in rows and cols

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -95,8 +95,7 @@ fn entrada(s: &mut [[u8; N]; N]) {
                 let digit: u32 = j.1.to_digit(10).unwrap();
                 //println!("Linea {}, Caracter {}, u32 {}, u8 {}", i.0, j.0, digit, (digit & 0xff) as u8);
                 s[i.0][j.0] = (digit & 0xff) as u8;     // u32 as 4 bytes, u8 as 1 byte (transform only last byte)
-            }
-        }    
+            }        }    
     }
 }
 
@@ -120,6 +119,80 @@ fn imprimir(s: [[u8; N]; N]) {
     println!("+-------+-------+-------+");   
 }
 
+fn pos(sudo: [[u8; N]; N], row: usize, col: usize) -> HashSet<u8> {
+
+    let mut p: HashSet<u8> = HashSet::new();
+    if sudo[row][col] == 0 {
+        let f = pendent_fila(sudo[row]);
+        let c = pendent_columna(sudo, col);
+        let s = pendent_sub(sudo, row, col);
+        let m: HashSet<u8>  = f.intersection(&c).cloned().collect();
+        //println!("Mig: {:?}", m);
+        p = m.intersection(&s).cloned().collect();
+    }
+
+    return p;
+} 
+
+fn restricc_fila(s: &mut [[u8; N]; N], row: usize) -> bool {     // Retorna 'true' si modifica el sudoku
+    let mut modified = false;
+
+    let mut p_f: [HashSet<u8>; N] = [HashSet::new(), HashSet::new(), HashSet::new(),
+        HashSet::new(), HashSet::new(), HashSet::new(),HashSet::new(), HashSet::new(), HashSet::new()];
+    
+    // Calcular les possibilitats de cada cel.la d'aquesta fila
+    for y in 0..N {
+        p_f[y] = pos(*s, row, y);
+    } 
+
+    // Check de cada cel.la la resta per veure si hi ha un nombre exclusiu 
+    for y in 0..9 {
+        let mut p: HashSet<u8> = p_f[y].iter().cloned().collect();
+        for col in 0..9 {
+            if (y != col) && !p.is_empty() {
+                p = p.difference(&p_f[col]).cloned().collect();
+            }    
+        }
+        if p.len() == 1 {
+            let number: u8 = p.drain().next().unwrap();
+            s[row][y] = number;
+            modified = true;            
+        }
+
+    } 
+
+    return modified;
+}
+
+fn restricc_col(s: &mut [[u8; N]; N], col: usize) -> bool {     // Retorna 'true' si modifica el sudoku
+    let mut modified = false;
+
+    let mut p_c: [HashSet<u8>; N] = [HashSet::new(), HashSet::new(), HashSet::new(),
+        HashSet::new(), HashSet::new(), HashSet::new(),HashSet::new(), HashSet::new(), HashSet::new()];
+    
+    // Calcular les possibilitats de cada cel.la d'aquesta fila
+    for x in 0..N {
+        p_c[x] = pos(*s, x, col);
+    } 
+
+    // Check de cada cel.la la resta per veure si hi ha un nombre exclusiu 
+    for x in 0..9 {
+        let mut p: HashSet<u8> = p_c[x].iter().cloned().collect();
+        for row in 0..9 {
+            if (x != row) && !p.is_empty() {
+                p = p.difference(&p_c[row]).cloned().collect();
+            }    
+        }
+        if p.len() == 1 {
+            let number: u8 = p.drain().next().unwrap();
+            s[x][col] = number;
+            modified = true;            
+        }
+
+    } 
+
+    return modified;
+}
 
 fn main() {
     
@@ -129,6 +202,7 @@ fn main() {
     println!("Inici:");
     imprimir(sudoku);
 
+/*
     let mut fi = false;
     let mut complet = true;
     while !fi {
@@ -137,13 +211,7 @@ fn main() {
         for i in 0..9 {
             for j in 0..9 {
                 if sudoku[i][j] == 0 {
-                    let f = pendent_fila(sudoku[i]);    //i
-                    let c = pendent_columna(sudoku, j); //j
-                    let s = pendent_sub(sudoku, i, j);  //i,j
-                    let m: HashSet<u8>  = f.intersection(&c).cloned().collect();
-                    //println!("Mig: {:?}", m);
-                    let mut p: HashSet<u8> = m.intersection(&s).cloned().collect();
-                    //println!("Possibilitats: {:?}", p);
+                    let mut p = pos(sudoku, i , j);
                     if p.len() == 1 {
                         let number: u8 = p.drain().next().unwrap();
                         //println!("OK {}", number);
@@ -159,23 +227,30 @@ fn main() {
             fi = true;
         }
     }
-
+*/
+    for i in 0..9 {
+        if restricc_fila(&mut sudoku, i) {
+            println!("S'HA MODIFICAT, LINEA {}", i);
+        }    
+    }
+    for i in 0..9 {
+        if restricc_col(&mut sudoku, i) {
+            println!("S'HA MODIFICAT, COLUMNA {}", i);
+        }    
+    }
     println!("Final:");
     imprimir(sudoku);
-
+/*
     if !complet {       // Encara hi ha cel.les buides (amb 0)
         println!("Sudoku no finalitzat...");
         for i in 0..9 {
             for j in 0..9 {
                 if sudoku[i][j] == 0 {
-                    let f = pendent_fila(sudoku[i]);    //i
-                    let c = pendent_columna(sudoku, j); //j
-                    let s = pendent_sub(sudoku, i, j);  //i,j
-                    let m: HashSet<u8> = f.intersection(&c).cloned().collect();
-                    let p: HashSet<u8> = m.intersection(&s).cloned().collect();
+                    let p = pos(sudoku, i, j);
                     println!("Possibilitats posició {}, {}: {:?}", i, j, p);
                 }
             }
         }        
-    }    
+    }
+*/     
 }
