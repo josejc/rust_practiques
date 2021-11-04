dbba991bc6212fc2a1e1110db5887d64c4890564
José J. Cabezas <josej.cabezas@gmail.com>
2021-11-04 22:11:26 +0100

    Add binary search recursive... TODO fix index in superior division of the vector

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -2,7 +2,7 @@
 
 //binary_search pag.9 - grokking algorithms
 //Only work in a ordered array ;-)
-fn binary_search (list: [i32; 5], item:i32) -> i32 {
+fn binary_search (list: &Vec<i32>, item:i32) -> i32 {
     let mut low = 0;
     let mut high = list.len()-1;
 
@@ -24,15 +24,50 @@ fn binary_search (list: [i32; 5], item:i32) -> i32 {
     -1
 }
 
-fn main() {
-    let my_list = [1, 3, 5, 7, 9];
+// binary_search_recursive, Exercise 4.3
+fn bsr (list: &Vec<i32>, item: i32) -> i32 {
+    let mid = list.len() / 2;
+    //println!("{:?}", list);
+    //println!("len:{}", list.len());
+    //println!("mid:{}", mid);
+    if list.len() == 0 {
+        return -1;
+    }
+    if list[mid] == item {
+        return mid as i32;
+    }
+    if list[mid] > item {
+        return bsr(&list[0..mid].to_vec(), item);
+    } else {
+        // TODO lost the lengh of the original vector
+        // The index solution is in the new vector...
+        // Fix... 
+        let n = list.len();
+        if (mid + 1) <= n {
+            return bsr(&list[mid+1..n].to_vec(), item);
+        } 
+    }
+    -1
+}
 
-    println!("{}", binary_search(my_list, 1));
-    println!("{}", binary_search(my_list, 3));
-    println!("{}", binary_search(my_list, 5));
-    println!("{}", binary_search(my_list, 7));
-    println!("{}", binary_search(my_list, 9));
-    println!("{}", binary_search(my_list, 6));
-    println!("{}", binary_search(my_list, 26));
-    println!("{}", binary_search(my_list, -3));
+fn main() {
+    let my_list = vec![1, 3, 5, 7, 9];
+
+    println!("{:?}", my_list);
+    println!("Binary search 1 at index: {}", binary_search(&my_list, 1));
+    println!("Binary search 3 at index: {}", binary_search(&my_list, 3));
+    println!("Binary search 5 at index: {}", binary_search(&my_list, 5));
+    println!("Binary search 7 at index: {}", binary_search(&my_list, 7));
+    println!("Binary search 9 at index: {}", binary_search(&my_list, 9));
+    println!("Binary search 6 at index: {}", binary_search(&my_list, 6));
+    println!("Binary search 26 at index: {}", binary_search(&my_list, 26));
+    println!("Binary search -3 at index: {}", binary_search(&my_list, -3));
+    println!("Binary search recursive 1 at index: {}", bsr(&my_list, 1));
+    println!("Binary search recursive 3 at index: {}", bsr(&my_list, 3));
+    println!("Binary search recursive 5 at index: {}", bsr(&my_list, 5));
+    println!("Binary search recursive 7 at index: {}", bsr(&my_list, 7));
+    println!("Binary search recursive 9 at index: {}", bsr(&my_list, 9));
+    println!("Binary search recursive 6 at index: {}", bsr(&my_list, 6));
+    println!("Binary search recursive 26 at index: {}", bsr(&my_list, 26));
+    println!("Binary search recursive -3 at index: {}", bsr(&my_list, -3));
 }
