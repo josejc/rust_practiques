6b7d7792d3cf2e2732175780b58a3dfeed5b2fe1
José J. Cabezas <josej.cabezas@gmail.com>
2021-11-14 18:59:11 +0100

    Dijkstra implementation Grokking Algorithms

---
+++ dijkstra/src/main.rs
@@ -0,0 +1,111 @@
+mod prelude {
+    pub use std::collections::HashMap;
+}
+
+use prelude::*;
+
+fn new_graph() -> std::collections::HashMap<String, HashMap<String, isize>> {
+    let mut graph: HashMap<String, HashMap<String, isize>> = HashMap::new();
+
+    // Insert Nodes and his weigh
+    let mut neighbors: HashMap<String, isize> = HashMap::new();
+    neighbors.insert("A".to_string(), 6);
+    neighbors.insert("B".to_string(), 2);
+    graph.insert("S".to_string(), neighbors);
+
+    let mut neighbors = HashMap::new();
+    neighbors.insert("F".to_string(), 1);
+    graph.insert("A".to_string(), neighbors);
+
+    let mut neighbors = HashMap::new();
+    neighbors.insert("A".to_string(), 3);
+    neighbors.insert("F".to_string(), 5);
+    graph.insert("B".to_string(), neighbors);
+
+    let neighbors = HashMap::new();
+    graph.insert("F".to_string(), neighbors);
+
+    graph 
+}
+
+fn ini_dijkstra(graph: &HashMap<String, HashMap<String, isize>>) -> (HashMap<String, isize>, HashMap<String, String>) {
+    let mut costs: HashMap<String, isize> = HashMap::new();
+    let mut parents: HashMap<String, String> = HashMap::new();
+
+    if let Some(c) = graph.get(&"S".to_string()) {
+        costs = c.clone();
+    }
+    //add node "fin" with weigh infinity if doesn't exists
+    if !costs.contains_key(&"F".to_string()) {
+        costs.insert("F".to_string(), isize::MAX);
+    }
+
+    // Initialice parents
+    for value in costs.iter() {
+        // value.0 = node, value.1 = cost
+        if *value.1 != isize::MAX {
+            parents.insert(value.0.to_string(), "S".to_string());
+        } else {
+            parents.insert(value.0.to_string(), "".to_string());
+        }
+    }
+
+    (costs, parents)
+}
+
+//p.139 Grokking algorithms
+fn find_lowest_cost_node(costs: &HashMap<String, isize>, procesed: &Vec<String>) -> String {
+    let mut lowest_cost = isize::MAX;
+    let mut lowest_cost_node = "".to_string();
+
+    for value in costs.iter() {
+        // value.0 = node, value.1 = cost
+        if *value.1 < lowest_cost && !procesed.contains(value.0) {
+            lowest_cost = *value.1;
+            lowest_cost_node = value.0.clone();
+        }
+    }
+
+    lowest_cost_node
+}
+
+//p.134 Grokking algoritms
+fn main() {
+    let g = new_graph();
+    let tuple: (HashMap<String, isize>, HashMap<String, String>) = ini_dijkstra(&g);
+    let mut costs: HashMap<String, isize> = tuple.0;
+    let mut parents: HashMap<String, String> = tuple.1;
+    let mut procesed: Vec<String> = vec![];
+    let mut cost: isize;
+    let mut neighbors: HashMap<String, isize>;
+
+    println!("Graph: {:?}", g);
+    println!("Costs: {:?}", costs);
+    println!("Parents: {:?}", parents);
+    println!("Node low cost: {}", find_lowest_cost_node(&costs, &procesed));
+
+    let mut node: String = find_lowest_cost_node(&costs, &procesed);
+    while !node.is_empty() {
+        if let Some(c) = costs.get(&node.to_string()) {
+            cost = *c;
+            if let Some(n) = g.get(&node.to_string()) {
+                neighbors = n.clone();
+                for n in neighbors.iter() {
+                    let new_cost = cost + *n.1;
+                    // Check if it's cheaper to get to this neigbor by going through this node
+                    if let Some(c) = costs.get(&n.0.to_string()) {
+                        if *c > new_cost {
+                            costs.insert(n.0.to_string(), new_cost);
+                            parents.insert(n.0.to_string(), node.to_string());
+                        }
+                    }
+                }
+                procesed.push(node);
+                node = find_lowest_cost_node(&costs, &procesed);
+            }
+        }
+    }
+    println!("---Costs: {:?}", costs);
+    println!("---Parents: {:?}", parents);
+    println!("---Node low cost: {}", find_lowest_cost_node(&costs, &procesed));
+}
